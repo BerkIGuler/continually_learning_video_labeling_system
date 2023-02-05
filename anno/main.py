26defdea7b4e57110b37116e712eeaf97cd86338
Berkay Guler <you@example.com>
2023-02-05 22:24:16 +0300

    Small modifications: - added config.yaml] - fixed a syntax error on main.py

--- anno/main.py
+++ anno/main.py
@@ -10,7 +10,7 @@ def main():
     # sample_video
     sample_video_path = os.path.join(cfg.config["VIDEO_DIR"], "test_vid.mp4")
 
-    annotation_temp.annotation_from_local_video(sample_video_path)
+    annotation.annotation_from_local_video(sample_video_path)
 
 
 if __name__ == "__main__":
