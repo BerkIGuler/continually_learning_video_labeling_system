53b1a303b5d5ea3486562eaa462c317db3cc2b1c
Berkay Guler <you@example.com>
2023-02-17 23:10:01 +0300

    - added requirement.txt

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import annotation
 import os
-
 import cfg
 
 
 def main():
     cfg.init_config()
     # sample_video
-    sample_video_path = os.path.join(cfg.config["VIDEO_DIR"], "seckin.mp4")
+    sample_video_path = os.path.join(cfg.config["VIDEO_DIR"], "test_vid.mp4")
     annotation.annotation_from_local_video(sample_video_path)
 
 
